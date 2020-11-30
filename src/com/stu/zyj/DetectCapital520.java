e2806860e825f0d232c744d757d5b2d50bb76c67
A struggling coder <56886686+RD-Joey@users.noreply.github.com>
2020-11-30 21:01:16 +0800

    第 520 题增加正则表达式的解法。

--- src/com/stu/zyj/DetectCapital520.java
+++ src/com/stu/zyj/DetectCapital520.java
@@ -2,6 +2,8 @@ package com.stu.zyj;
 
 public class DetectCapital520 {
     public boolean detectCapitalUse(String word) {
+        //return word.matches("[A-Z]*|.[a-z]*");
+
         if(word.length() < 2) return true;
         if(word.charAt(0) >= 97) return word.toLowerCase().equals(word);
         if(word.charAt(1) >= 97) return word.substring(1).toLowerCase().equals(word.substring(1));
